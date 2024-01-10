3220d293ba2b68c1e5f2fdf43610116ed98edbdf
JerryShum <jerryshum2636@gmail.com>
2024-01-10 17:47:26 -0500

    feat: added methods for appending values for calculator

--- script.js
+++ script.js
@@ -5,10 +5,65 @@ let operator = null;
 
 // Selecting / Creating DOM elements
 const calcDisplayElement = document.querySelector('.calc-display');
-const calcNumbersButtons = document.querySelector('.calc-num-btns');
-const calcOperationButtons = document.querySelector('.calc-operations-btns');
+const calcBtnElements = document.querySelectorAll('.btn');
 
+calcBtnElements.forEach((btn) => btn.addEventListener('click', updateDisplay));
 
+function updateDisplay() {
+    let value = this.value;
+
+    if (/[0-9, . ]/.test(value)) {
+        appendNum(value);
+
+    } else if (value == "CLEAR") {
+        console.log("clear");
+        clear();
+    } else if (value == "DELETE") {
+        console.log('delete');
+    }
+    else if (value == '=') {
+        console.log('equals');
+    }
+    else {
+        console.log('OTHER');
+        operator =
+            calcDisplayElement.textContent += ` ${value} `;
+    }
+}
+
+function appendNum(value) {
+    if (value === '.' && firstNum.includes('.')) {
+        return;
+    }
+
+    if (operator == null) {
+        firstNum += value;
+    } else if (operator != null) {
+        secondNum += value;
+    }
+    console.log(firstNum);
+    console.log(secondNum);
+    calcDisplayElement.textContent += `${value}`
+}
+
+function appendOperator(value) {
+    if (operator != null) {
+        // solve if there already exists an operator 
+        // switch to num2
+        return;
+    }
+
+    operator = value;
+
+}
+
+function clear() {
+    firstNum = '';
+    secondNum = '';
+    operator = null;
+
+    calcDisplayElement.textContent = '';
+}
 
 // Operation Functions
 function add(a, b) {
