3818ba2dfaaa4148b17d7058a3f912e71b19b618
JerryShum <jerryshum2636@gmail.com>
2024-01-09 23:38:16 -0500

    feat: created calculator class and some methods

---
+++ script.js
@@ -0,0 +1,25 @@
+class calculator {
+
+    constructor(num1Value, num2Value) {
+        this.num1Value = num1Value;
+        this.num2Value = num2Value;
+    }
+
+    add(num1Value, num2Value) {
+        return num1Value + num2Value;
+    };
+
+    subtract(num1Value, num2Value) {
+        return num1Value - num2Value;
+    };
+
+    multiply(num1Value, num2Value) {
+        return num1Value * num2Value;
+    };
+
+    divide(num1Value, num2Value) {
+        return num1Value / num2Value;
+    };
+
+
+}
