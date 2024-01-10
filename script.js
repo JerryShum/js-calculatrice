0073bdc3349aeb368ca83bad5c8a91a9946491b1
JerryShum <jerryshum2636@gmail.com>
2024-01-09 23:47:40 -0500

    feat: defined variables (firstNum, secondNum, operator)
    and also defined operate() function

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+// Defining Variables
+let firstNum = '';
+let secondNum = '';
+let operator = null;
 
+// Operation Functions
 function add(a, b) {
     return a + b;
 };
@@ -14,3 +19,17 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 };
+
+// Operate Function
+function operate(a, b, operator) {
+    switch (operator) {
+        case '+':
+            return add(a, b)
+        case '-':
+            return subtract(a, b)
+        case '*':
+            return multiply(a, b)
+        case '/':
+            return divide(a, b)
+    }
+}
